520d2497ce058674753ee6b83860622b057a5fc9
jameskhamilton <james.k.hamilton@outlook.com>
2023-11-28 13:41:37 +0000

    returns dataset

--- inverter dataset.py
+++ inverter dataset.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import asyncio
 import solis_functions as sf
 
 filePath = 'C:\\test\\security.json'
@@ -14,38 +15,63 @@ stationId = securityData['stationId']
 url = 'https://www.soliscloud.com:13333'
 resource = "/v1/api/inveterList"
 
-body = f'{{"stationId":"{stationId}"}}'
+async def dataset() -> str:
+    """
+    Purpose:
+    - makes API call to obtain the full JSON result from inverterList
 
-header = { "Content-MD5":sf.base64Hash(body),
-            "Content-Type":sf.contentType(),
-            "Date":sf.currentDateTime(0),
-            "Authorization":sf.authValue(keyId, secretKey, body, resource)
-            }
+    Returns:
+    - JSON string
+    """
+    body = f'{{"stationId":"{stationId}"}}'
 
-req = url + resource
-result = requests.post(req, data=body, headers=header)
+    header = { "Content-MD5":sf.base64Hash(body),
+                "Content-Type":sf.contentType(),
+                "Date":sf.currentDateTime(0),
+                "Authorization":sf.authValue(keyId, secretKey, body, resource)
+                }
 
-jsonData = result.json()
+    req = url + resource
+    result = requests.post(req, data=body, headers=header)
 
-records = []
+    jsonData = result.json()
+    return jsonData
 
-for record in jsonData['data']['page']['records']:
+async def main() -> list:
+    """
+    Returns:
+    - list containing
+        - Inverter ID
+        - Data Timestamp (when the datalogger made the dataset available, following values are all as at this point in time)
+        - SoC
+        - Battery Discharge KwH
+        - PV Yield KwH
+        - Grid Usage KwH
+        - House Load KwH
+    """
+    records = []
+    jsonData = await dataset()
 
-    recordId = record['id']
-    recordTimestamp = record['dataTimestampStr']
-    recordSoC = record['batteryCapacitySoc']
-    recordDischarge = -record['batteryPower'] #flipping negation to make discharge the positive value, charging a negative
-    recordPVYield = record['pac']
-    recordGridUsage = -record['psum'] #flipping negation to make grid usage a positive, sending to grid a negative
+    for record in jsonData['data']['page']['records']:
 
-records.append({
-        'Inverter ID': recordId,
-        'Data Timestamp': recordTimestamp,
-        'SoC': recordSoC,
-        'Battery Discharge KwH': recordDischarge,
-        'PV Yield KwH': recordPVYield,
-        'Grid Usage KwH': recordGridUsage,
-        'House Load KwH': recordDischarge + recordPVYield + recordGridUsage
-})
+        recordId = record['id']
+        recordTimestamp = record['dataTimestampStr']
+        recordSoC = record['batteryCapacitySoc']
+        recordDischarge = -record['batteryPower'] #flipping negation to make discharge the positive value, charging a negative
+        recordPVYield = record['pac']
+        recordGridUsage = -record['psum'] #flipping negation to make grid usage a positive, sending to grid a negative
 
+    records.append({
+            'Inverter ID': recordId,
+            'Data Timestamp': recordTimestamp,
+            'SoC': recordSoC,
+            'Battery Discharge KwH': recordDischarge,
+            'PV Yield KwH': recordPVYield,
+            'Grid Usage KwH': recordGridUsage,
+            'House Load KwH': recordDischarge + recordPVYield + recordGridUsage
+    })
+
+    return records
+
+records = asyncio.run(main())
 print(records)
